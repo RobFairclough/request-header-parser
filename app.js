1884a7e7dcf9ab2a076665c1bb00a03952303c44
Rob Fairclough <rob.a.fairclough@gmail.com>
2018-12-19 09:34:42 +0000

    update descrip

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const { sendUserData } = require("./controllers/controller");
 
 app.get("/", (req, res, send) => {
   // homepage
+  res.send({
+    msg: `Usage: /api/whoami will return some details, your ip, software and preferred languages`
+  });
 });
 
 app.get("/api/whoami", sendUserData);
